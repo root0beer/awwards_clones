0de598340d2d3a7630cb1738b18f081e4b18a8b8
root0beer <tanyajackdaw@gmail.com>
2023-10-23 22:42:51 +0300

    finalizing the proj

--- components/Pixeliz/ImageItem/ImageItem.jsx
+++ components/Pixeliz/ImageItem/ImageItem.jsx
@@ -1,70 +1,82 @@
-// 'use client'
-import styles from './ImageItem.module.scss';
-import { useRef, useState, useEffect } from 'react';
-import NextImage from 'next/image';
-import { useInView } from 'react-intersection-observer';
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./ImageItem.module.scss";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
 
-export default function Index({src, src10}) {
+const ImageItem = ({ src, src10 }) => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const canvas = useRef(null);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
 
-    const [dimension, setDimension] = useState({width: 0, height: 0});
-    const canvas = useRef(null);
+  useEffect(() => {
+    //lazy loading
+    if (inView && dimensions.height > 0) {
+      const image = document.createElement("img");
+      image.onload = () => {
+        setTimeout(() => {
+          animate(image);
+        }, 150);
+      };
+      image.src = src;
+    }
+  }, [inView, dimensions]);
+  const drawImage = (image) => {
+    const ctx = canvas.current.getContext("2d", { willReadFrequently: true });
+    ctx.drawImage(image, 0, 0, dimensions.width, dimensions.height);
+  };
 
-    const { ref, inView } = useInView({
-        threshold: 0.75,
-        triggerOnce: true
-    });
+  const animate = (image, size = 20) => {
+    drawImage(image);
+    if (size < 5) return;
 
-    useEffect(() => {
-        if(inView && dimension.width > 0){
-            const image = new Image();
-            image.onload = () => {
-                setTimeout( () => {
-                    animate(image);
-                }, 150)
-            }
-            image.src = src
-        }
-    }, [inView, dimension])
-
-    const drawImage = (image) => {
-        const ctx = canvas.current.getContext("2d", {willReadFrequently: true});
-        ctx.drawImage(image, 0, 0, dimension.width, dimension.height);
-        
+    const w = dimensions.width;
+    const h = dimensions.height;
+    const ctx = canvas.current.getContext("2d", { willReadFrequently: true });
+    const pixelArr = ctx.getImageData(0, 0, w, h).data;
+    for (let y = 0; y < h; y += size) {
+      for (let x = 0; x < w; x += size) {
+        let pos = (x + y * w) * 4;
+        ctx.fillStyle =
+          "rgba(" +
+          pixelArr[pos] +
+          "," +
+          pixelArr[pos + 1] +
+          "," +
+          pixelArr[pos + 2] +
+          "," +
+          pixelArr[pos + 3] +
+          ")";
+        ctx.fillRect(x, y, size, size);
+      }
     }
 
-    const animate = (image, size=20) => {
-        drawImage(image);
-        if(size < 5) return;
-        
-        const w = dimension.width;
-        const h = dimension.height;
-        const ctx = canvas.current.getContext("2d", {willReadFrequently: true});
-        const pixelArr = ctx.getImageData(0, 0, w, h).data;
-        for (let y = 0; y < h; y += size) {
-          for (let x = 0; x < w; x += size) {
-            let pos = (x + y * w) * 4;
-            ctx.fillStyle = "rgba(" + pixelArr[pos] + "," + pixelArr[pos + 1] + "," + pixelArr[pos + 2] + "," + pixelArr[pos + 3] + ")";
-            ctx.fillRect(x, y, size, size);
-          }
-        }
+    setTimeout(() => {
+      animate(image, size/2);
+    }, 150);
+  };
+  return (
+    <div className={styles.imageContainer}>
+      <Image
+        ref={ref}
+        src={src10}
+        alt={src10 + "image"}
+        width={0}
+        height={0}
+        priority={true}
+        onLoadingComplete={(e) => {
+          setDimensions({ width: e.width, height: e.height });
+        }}
+      />
+      <canvas
+        ref={canvas}
+        width={dimensions.width}
+        height={dimensions.height}
+      ></canvas>
+    </div>
+  );
+};
 
-        setTimeout( () => {
-            animate(image, size/2);
-        }, 150)
-    }
-
-    return (
-        <div className={styles.ImageContainer}>
-            <NextImage 
-                ref={ref} 
-                src={src10}
-                width={0}
-                height={0}
-                onLoadingComplete={(e) => {setDimension({width: e.width, height: e.height})}}
-                priority={true}
-                alt="image"
-            />            
-            <canvas ref={canvas} width={dimension.width} height={dimension.height}></canvas>
-        </div>
-    )
-}
+export default ImageItem;
