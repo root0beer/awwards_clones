9428b1cb51aac586a8d5e473c482e060ce558fbc
root0beer <tanyajackdaw@gmail.com>
2023-10-23 15:42:52 +0300

    removing the trail

--- components/CursorT/CursorT.jsx
+++ components/CursorT/CursorT.jsx
@@ -7,7 +7,7 @@ const CursorT = () => {
   const body = useRef();
 
   const managMouseMove = (e) => {
-    const {clientX, clientY} = e;
+    const { clientX, clientY } = e;
     const bodyBound = body.current.getBoundingClientRect();
     const x = clientX - bodyBound.x;
     const y = clientY - bodyBound.y;
@@ -22,11 +22,28 @@ const CursorT = () => {
     div.style.top = y + "px";
     div.style.left = x + "px";
     body.current.append(div);
+    if (body.current.childNodes.length > 25) {
+      erase();
+    } else {
+      setTimeout(() => {
+        erase();
+      }, 1500);
+    }
+  };
+
+  const erase = () => {
+    body.current.removeChild(body.current.childNodes[1]);
   };
 
   return (
     <div className={styles.container}>
-      <div ref={body} onMouseMove={(e) => {managMouseMove(e)}} className={styles.body}>
+      <div
+        ref={body}
+        onMouseMove={(e) => {
+          managMouseMove(e);
+        }}
+        className={styles.body}
+      >
         <img ref={img} src="smiley.svg"></img>
       </div>
     </div>
