103baf3d245f5cd00eeeb8c5b621ebc4f158e680
root0beer <tanyajackdaw@gmail.com>
2023-10-24 22:45:08 +0300

    aniamting in and out

--- components/TextMask/TextMask.jsx
+++ components/TextMask/TextMask.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styles from "./TextMask.module.scss";
+import { useInView, motion } from "framer-motion";
 
 const phrases = [
   "It is a long established fact",
@@ -8,13 +9,30 @@ const phrases = [
   "when looking at this layout",
 ];
 
-const PieceText = () => {
+const PieceText = ({ phrases }) => {
+  const animate = {
+    initial: { y: "100%" },
+    open: (i) => ({
+      y: "0%",
+      transition: { duration: 0.75, delay: i * 0.2, ease: [0.33, 1, 0.68, 1] },
+    }),
+  };
+  const body = useRef(null);
+  const isInView = useInView(body, { once: true, margin: "-10%" });
+
   return (
-    <div className={styles.body}>
+    <div ref={body} className={styles.body}>
       {phrases.map((phrase, index) => {
         return (
           <div key={index} className={styles.linemask}>
-            <p>{phrase}</p>
+            <motion.p
+              custom={index}
+              variants={animate}
+              initial="initial"
+              animate={isInView ? "open" : ""}
+            >
+              {phrase}
+            </motion.p>
           </div>
         );
       })}
@@ -25,10 +43,10 @@ const PieceText = () => {
 const TextMask = () => {
   return (
     <div className={styles.main}>
-      <PieceText />
-      <PieceText />
-      <PieceText />
-      <PieceText />
+      <PieceText phrases={phrases} />
+      <PieceText phrases={phrases} />
+      <PieceText phrases={phrases} />
+      <PieceText phrases={phrases} />
     </div>
   );
 };
