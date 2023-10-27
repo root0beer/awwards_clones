4d257b82561ba94b2052984db9d36b0787b259a9
root0beer <tanyajackdaw@gmail.com>
2023-10-27 19:48:33 +0300

    animating nav menu as a block

--- components/CurvedMenu/CurvedMenu.jsx
+++ components/CurvedMenu/CurvedMenu.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styles from "./CurvedMenu.module.scss";
 import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import { menuSlide } from "./anim";
 
 const CurvedMenu = () => {
   const navItems = [
@@ -30,20 +32,34 @@ const CurvedMenu = () => {
           className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
         ></div>
       </div>
-      {isActive && (
-        <div className={styles.menu}>
-          <div className={styles.body}>
-            <div className={styles.nav}>
-              <div className={styles.header}>
-                <p>Navigation</p>
+      <AnimatePresence mode="wait">
+        {isActive && (
+          <motion.div
+            variants={menuSlide}
+            animate="enter"
+            exit="exit"
+            initial="initial"
+            className={styles.menu}
+          >
+            <div className={styles.body}>
+              <div className={styles.nav}>
+                <div className={styles.header}>
+                  <p>Navigation</p>
+                </div>
+                {navItems.map((item, index) => {
+                  return <Link href={item.href}>{item.title}</Link>;
+                })}
               </div>
-              {navItems.map((item, index) => {
-                return <Link href={item.href}>{item.title}</Link>;
-              })}
             </div>
-          </div>
-        </div>
-      )}
+            <div className={styles.footer}>
+              <a>Awwards</a>
+              <a>Instagram</a>
+              <a>Dribble</a>
+              <a>LinkedIn</a>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
