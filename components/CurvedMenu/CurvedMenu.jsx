f78ffb1707a62b342570b4fa5c92200b73382a97
root0beer <tanyajackdaw@gmail.com>
2023-10-27 19:17:23 +0300

    navbar button

--- components/CurvedMenu/CurvedMenu.jsx
+++ components/CurvedMenu/CurvedMenu.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./CurvedMenu.module.scss";
 
 const CurvedMenu = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div>CurvedMenu</div>
-  )
+    <>
+      <div onClick={() => setIsActive(!isActive)} className={styles.button}>
+        <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}>
+
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CurvedMenu;
