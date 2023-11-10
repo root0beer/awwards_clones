f6a48308f0418c2bb9ad4a83d40691224453c414
root0beer <tanyajackdaw@gmail.com>
2023-11-10 17:00:00 +0200

    git add .

--- components/MouseMoveGalery/MouseMoveGalery.jsx
+++ components/MouseMoveGalery/MouseMoveGalery.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from "./MouseMoveGalery.module.scss";
 
 const MouseMoveGalery = () => {
+    
+    let currentIndex = 0;
+    let collection = [];
+
+    const manageMouseMove = (e) => {
+        const { clientX, clientY } = e;
+        mouseMove(clientX, clientY);
+    };
+
+    const mouseMove = (x, y) => {
+        const targetImage = collection[currentIndex].current;
+        targetImage.style.display = "block";
+        targetImage.style.left = x + "px";
+        targetImage.style.top = y + "px";
+        currentIndex++;
+    };
+
   return (
-    <main className={styles.main}>
+    <main onMouseMove={(e) => {manageMouseMove(e)}} className={styles.main}>
         {
             [...Array(19).keys()].map((_, index) => {
-                return <img key={index} src={`/cursorgalery/`}></img>
+                const ref = useRef(null);
+                collection.push(ref);
+                return <img ref={ref} key={index} src={`/cursorgalery/${index}.jpg`}></img>
             })
         }
     </main>
