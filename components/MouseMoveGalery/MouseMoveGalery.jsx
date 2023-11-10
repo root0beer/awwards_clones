6ad4546085e281cca31953b0a2bdcda4691e06c5
root0beer <tanyajackdaw@gmail.com>
2023-11-10 17:00:18 +0200

    aminating the image

--- components/MouseMoveGalery/MouseMoveGalery.jsx
+++ components/MouseMoveGalery/MouseMoveGalery.jsx
@@ -1,35 +1,46 @@
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
 import styles from "./MouseMoveGalery.module.scss";
 
 const MouseMoveGalery = () => {
-    
-    let currentIndex = 0;
-    let collection = [];
+  let currentIndex = 0;
+  let collection = [];
+  let step = 0;
 
-    const manageMouseMove = (e) => {
-        const { clientX, clientY } = e;
-        mouseMove(clientX, clientY);
-    };
+  const manageMouseMove = (e) => {
+    const { clientX, clientY, movementX, movementY } = e;
 
-    const mouseMove = (x, y) => {
-        const targetImage = collection[currentIndex].current;
-        targetImage.style.display = "block";
-        targetImage.style.left = x + "px";
-        targetImage.style.top = y + "px";
-        currentIndex++;
-    };
+    step += Math.abs(movementX) + Math.abs(movementY);
+
+    //if it passes 150 step
+    if (step >= 150 * currentIndex) {
+      mouseMove(clientX, clientY);
+    }
+  };
+
+  const mouseMove = (x, y) => {
+    const targetImage = collection[currentIndex].current;
+    targetImage.style.display = "block";
+    targetImage.style.left = x + "px";
+    targetImage.style.top = y + "px";
+    currentIndex++;
+  };
 
   return (
-    <main onMouseMove={(e) => {manageMouseMove(e)}} className={styles.main}>
-        {
-            [...Array(19).keys()].map((_, index) => {
-                const ref = useRef(null);
-                collection.push(ref);
-                return <img ref={ref} key={index} src={`/cursorgalery/${index}.jpg`}></img>
-            })
-        }
+    <main
+      onMouseMove={(e) => {
+        manageMouseMove(e);
+      }}
+      className={styles.main}
+    >
+      {[...Array(19).keys()].map((_, index) => {
+        const ref = useRef(null);
+        collection.push(ref);
+        return (
+          <img ref={ref} key={index} src={`/cursorgalery/${index}.jpg`}></img>
+        );
+      })}
     </main>
-  )
-}
+  );
+};
 
-export default MouseMoveGalery
+export default MouseMoveGalery;
