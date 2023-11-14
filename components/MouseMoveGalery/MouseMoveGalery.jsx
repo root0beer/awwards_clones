c63e66561cde6b846aa58e81fb3417ff4b40b962
root0beer <tanyajackdaw@gmail.com>
2023-11-14 07:48:29 +0200

    fixing a bug of max images not working

--- components/MouseMoveGalery/MouseMoveGalery.jsx
+++ components/MouseMoveGalery/MouseMoveGalery.jsx
@@ -2,67 +2,64 @@ import React, { useRef } from "react";
 import styles from "./MouseMoveGalery.module.scss";
 
 const MouseMoveGalery = () => {
-  let currentIndex = 0;
-  let refs = [];
   let steps = 0;
-  let maxNumberOfImages = 3;
-  let nbofImages = 0;
+  let currentIndex = 0;
+  let nbOfImages = 0;
+  let maxNumberOfImages = 8;
+  let refs = [];
 
   const manageMouseMove = (e) => {
     const { clientX, clientY, movementX, movementY } = e;
 
-    steps += Math.abs(movementX) + Math.abs(movementY);
+    steps+= Math.abs(movementX) + Math.abs(movementY);
 
-    //if it passes 150 steps
-    if (steps >= currentIndex * 150) {
-      mouseMove(clientX, clientY);
+    if(steps >= currentIndex * 150){
+      moveImage(clientX, clientY);
 
-      if (nbofImages == maxNumberOfImages) {
+      if(nbOfImages == maxNumberOfImages){
         removeImage();
-      };
-    };
-
-    //looping the images
-    if (currentIndex == refs.length) {
-        currentIndex = 0;
-        steps = -150;
-    };
-  };
-
-  const removeImage = () => {
-    const images = getImages();
-    images[0].style.dispay = "none";
-    nbofImages--;
-  };
-
-  const mouseMove = (x, y) => {
-    const targetImage = refs[currentIndex].current;
-    targetImage.style.left = x + "px";
-    targetImage.style.top = y + "px";
-    targetImage.style.display = "block";
-    currentIndex++;
-    nbofImages++;
+      }
+    }
     
-    resetZIndex(); 
-  };
+    if(currentIndex == refs.length){
+      currentIndex = 0;
+      steps = -150;
+    }
+  }
 
-  const resetZIndex = () => {
-    const images = getImages();
+  const moveImage = (x, y) => {
+    const currentImage = refs[currentIndex].current;
+    currentImage.style.left = x + "px";
+    currentImage.style.top = y + "px";
+    currentImage.style.display = "block";
+    currentIndex++;
+    nbOfImages++;
+    setZIndex()
+  }
+
+  const setZIndex = () => {
+    const images = getCurrentImages();
     for(let i = 0 ; i < images.length ; i++){
       images[i].style.zIndex = i;
-    };
-  };
+    }
+  }
 
-  const getImages = () => {
-    let images = [];
-    const indexOfFirstImage = currentIndex - nbofImages;
-    for (let i = indexOfFirstImage; i < currentIndex; i++) {
-        let targetIndex = i;
-        if (targetIndex <0) targetIndex +=refs.length;
-        images.push(refs[targetIndex].current);
-    };
+  const removeImage = () => {
+    const images = getCurrentImages();
+    images[0].style.display = "none";
+    nbOfImages--;
+  }
+
+  const getCurrentImages = () => {
+    let images = []
+    let indexOfFirst = currentIndex - nbOfImages;
+    for(let i = indexOfFirst ; i < currentIndex ; i++){
+      let targetIndex = i;
+      if(targetIndex < 0) targetIndex += refs.length
+      images.push(refs[targetIndex].current);
+    }
     return images;
-};
+  }
 
   return (
     <main
