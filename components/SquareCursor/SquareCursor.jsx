e578218a357aea35fd7b68c25ccea6ffa6573b5f
root0beer <tanyajackdaw@gmail.com>
2023-11-14 08:10:19 +0200

    basic html and css

--- components/SquareCursor/SquareCursor.jsx
+++ components/SquareCursor/SquareCursor.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 import styles from "./SquareCursor.module.scss";
 
 const SquareCursor = () => {
   return (
-    <div>SquareCursor</div>
-  )
+    <main className={styles.main}>
+      <div className={styles.body}>
+        <p>We specialize in turning space into complexe shapes</p>
+      </div>
+      <div className={styles.grid}>
+        {[...Array(20).keys()].map((_, index) => {
+          return <div key={index} className={styles.column}></div>;
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default SquareCursor;
