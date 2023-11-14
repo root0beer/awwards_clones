3ff455b3fb381ec4372b3dc197d29ba8c21d721b
root0beer <tanyajackdaw@gmail.com>
2023-11-14 08:15:01 +0200

    creating blocks

--- components/SquareCursor/SquareCursor.jsx
+++ components/SquareCursor/SquareCursor.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import styles from "./SquareCursor.module.scss";
 
 const SquareCursor = () => {
+
+    const getBlocks = () => {
+        const blockSize = window.innerWidth * 0.02;
+        const nbOfBlocks = Math.ceil(window.innerHeight / blockSize);
+        return [...Array(nbOfBlocks).keys()].map((_, index) => {
+            return <div key={`b_${index}`} className={styles.block}></div>
+        })
+    };
+
   return (
     <main className={styles.main}>
       <div className={styles.body}>
         <p>We specialize in turning space into complexe shapes</p>
       </div>
       <div className={styles.grid}>
-        {[...Array(20).keys()].map((_, index) => {
-          return <div key={index} className={styles.column}></div>;
+        {[...Array(50).keys()].map((_, index) => {
+          return <div key={index} className={styles.column}>
+            {
+                getBlocks()
+            }
+          </div>;
         })}
       </div>
     </main>
