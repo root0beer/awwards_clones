b5995fcbf9498e5f7ef42a0b65fcf1bc156ff40a
root0beer <tanyajackdaw@gmail.com>
2023-11-23 12:22:40 +0200

    splitting phrase

--- components/TextGradient/TextGradient.jsx
+++ components/TextGradient/TextGradient.jsx
@@ -4,8 +4,32 @@ import styles from "./TextGradient.module.scss";
 const TextGradient = () => {
 
     const phrase = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
+
+    const splitWords = () => {
+        let body = [];
+        phrase.split(" ").forEach((word, index) => {
+            const letters = splitLetters(word);
+            body.push(<p key={`word_${index}`}>{letters}</p>)
+        });
+        return body;
+    };
+
+    const splitLetters = (word) => {
+        let letters = [];
+        word.split("").forEach((letter, index) => {
+            letters.push(<span key={`letter_${index}`}>{letter}</span>);
+        });
+        return letters;
+    };
+
   return (
-    <main className={styles.main}></main>
+    <main className={styles.main}>
+        <div className={styles.body}>
+            {
+                splitWords()
+            }
+        </div>
+    </main>
   )
 };
 
