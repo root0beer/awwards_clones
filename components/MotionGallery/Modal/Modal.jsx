84c0a7ef918471fa5f104c489de529be4c90b719
root0beer <tanyajackdaw@gmail.com>
2023-11-23 10:51:25 +0200

    enter exit with framer

--- components/MotionGallery/Modal/Modal.jsx
+++ components/MotionGallery/Modal/Modal.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from "./Modal.module.scss";
 import Image from 'next/image';
+import {motion} from "framer-motion"
+
+const scaleAnimation = {
+    initial: {scale: 0, x: "-50%", y: "-50%"},
+    open: {scale: 1, x: "-50%", y: "-50%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
+    closed: {scale: 0, x: "-50%", y: "-50%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
+}
 
 const Modal = ({projects, modal}) => {
 
     const {active, index} = modal;
 
   return (
-    <div className={styles.modalContainer}>
+    <motion.div variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.modalContainer}>
         <div style={{top: index * -100 + "%"}} className={styles.modalSlider}>
             {
                 projects.map((project, index) => {
@@ -23,7 +30,7 @@ const Modal = ({projects, modal}) => {
                 })
             }
         </div>
-    </div>
+    </motion.div>
   )
 };
 
