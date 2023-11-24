fbfa5f04f9fd8302639b32e2bfbb0a2ce62fe838
root0beer <tanyajackdaw@gmail.com>
2023-11-24 12:48:30 +0200

    bug fixed?

--- components/InfiniteText/InfiniteText.jsx
+++ components/InfiniteText/InfiniteText.jsx
@@ -7,38 +7,41 @@ import { ScrollTrigger } from 'gsap/all';
 const InfiniteText = () => {
 
     const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  let xPercent = 0;
-  let direction = 1;
+    const secondText = useRef(null);
+    const slider = useRef(null);
+    let xPercent = 0;
+    let direction = -1;
 
-  useEffect( () => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
-      },
-      x: "-500px",
-    })
-    requestAnimationFrame(animate);
-  }, [])
 
-  const animate = () => {
-    if(xPercent < -100){
-      xPercent = 0;
-    }
-    else if(xPercent > 0){
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        requestAnimationFrame(animation);
+        
+        gsap.to(slider.current, {
+            scrollTrigger: {
+                //document element means start from the top of the page
+                trigger: document.documentElement,
+                start: 0,
+                end: window.innerHeight,
+                scrub: true,
+                onUpdate: e => direction = e.direction * -1,
+            },
+            x: "-300px",
+        });
+    }, []);
+
+    const animation = () => {
+        if (xPercent <= -100) {
+            xPercent = 0;
+        };
+        if (xPercent > 0) {
+            xPercent = -100;
+        }
+        gsap.set(firstText.current, {xPercent: xPercent});
+        gsap.set(secondText.current, {xPercent: xPercent});
+        xPercent += 0.25 * direction;
+        requestAnimationFrame(animation);
+    };
 
   return (
     <main className={styles.main}>
