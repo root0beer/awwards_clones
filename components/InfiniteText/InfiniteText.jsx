4c904b460ef641939ad3960a5b72451ad02ce2c7
root0beer <tanyajackdaw@gmail.com>
2023-11-24 12:46:04 +0200

    scroll interaction

--- components/InfiniteText/InfiniteText.jsx
+++ components/InfiniteText/InfiniteText.jsx
@@ -2,30 +2,43 @@ import React, {useEffect, useRef} from 'react';
 import styles from "./InfiniteText.module.scss";
 import Image from 'next/image';
 import gsap from "gsap";
+import { ScrollTrigger } from 'gsap/all';
 
 const InfiniteText = () => {
 
     const firstText = useRef(null);
-    const secondText = useRef(null);
-    let xPercent = 0;
-    let direction = -1;
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = 1;
 
-    useEffect(() => {
-        requestAnimationFrame(animation);
-    }, []);
+  useEffect( () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: e => direction = e.direction * -1
+      },
+      x: "-500px",
+    })
+    requestAnimationFrame(animate);
+  }, [])
 
-    const animation = () => {
-        if (xPercent <= -100) {
-            xPercent = 0;
-        };
-        if (xPercent > 0) {
-            xPercent = -100;
-        }
-        gsap.set(firstText.current, {xPercent: xPercent});
-        gsap.set(secondText.current, {xPercent: xPercent});
-        xPercent += 0.25 * direction;
-        requestAnimationFrame(animation);
-    };
+  const animate = () => {
+    if(xPercent < -100){
+      xPercent = 0;
+    }
+    else if(xPercent > 0){
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  }
 
   return (
     <main className={styles.main}>
@@ -35,7 +48,7 @@ const InfiniteText = () => {
         fill={true}
         />
         <div className={styles.sliderContainer}>
-            <div className={styles.slider}>
+            <div ref={slider} className={styles.slider}>
                 <p ref={firstText}>Freelance Photographer -</p>
                 <p ref={secondText}>Freelance Photographer -</p>
             </div>
