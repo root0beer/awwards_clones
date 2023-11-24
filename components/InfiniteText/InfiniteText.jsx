3b555ed3161f0546a9cc7b96312ddeb235185ba4
root0beer <tanyajackdaw@gmail.com>
2023-11-24 12:30:03 +0200

    html and css and  moving the text

--- components/InfiniteText/InfiniteText.jsx
+++ components/InfiniteText/InfiniteText.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from "./InfiniteText.module.scss";
+import Image from 'next/image';
+import gsap from "gsap";
 
 const InfiniteText = () => {
+
+    const firstText = useRef(null);
+    const secondText = useRef(null);
+    let xPercent = 0;
+    let direction = -1;
+
+    useEffect(() => {
+        requestAnimationFrame(animation);
+    }, []);
+
+    const animation = () => {
+        gsap.set(firstText.current, {xPercent: xPercent});
+        gsap.set(secondText.current, {xPercent: xPercent});
+        xPercent += 0.1 * direction;
+        requestAnimationFrame(animation);
+    };
+
   return (
-    <div>InfiniteText</div>
+    <main className={styles.main}>
+        <Image
+        src={"/inftext.jpg"}
+        alt={"infimage"}
+        fill={true}
+        />
+        <div className={styles.sliderContainer}>
+            <div className={styles.slider}>
+                <p ref={firstText}>Freelance Photographer -</p>
+                <p ref={secondText}>Freelance Photographer -</p>
+            </div>
+        </div>
+    </main>
   )
 };
 
