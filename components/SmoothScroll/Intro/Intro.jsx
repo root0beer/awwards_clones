9156f90682360c008ebad6dad95a14b441ee862f
root0beer <tanyajackdaw@gmail.com>
2023-11-28 17:47:27 +0200

    intro component html

---
+++ components/SmoothScroll/Intro/Intro.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Intro.module.scss";
+import Image from "next/image";
+
+const Intro = () => {
+  return (
+    <div className={styles.intro}>
+      <div className={styles.backgroundImage}>
+        <Image
+          src={"/smoothscroll/background.jpg"}
+          fill={true}
+          alt="background image"
+        />
+      </div>
+      <div className={styles.introContainer}>
+        <div className={styles.introImage}>
+          <Image
+            src={"/smoothscroll/intro.png"}
+            fill={true}
+            alt="background image2"
+          />
+        </div>
+        <h1>Smooth Scroll</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
