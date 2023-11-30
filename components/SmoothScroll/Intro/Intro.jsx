e4220bb46d35f21f320cfa8e701764719cb4a849
root0beer <tanyajackdaw@gmail.com>
2023-11-30 18:45:39 +0200

    wip on animation gsap scroll and fixing bug not parallaxing

--- components/SmoothScroll/Intro/Intro.jsx
+++ components/SmoothScroll/Intro/Intro.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styles from "./Intro.module.scss";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Intro = () => {
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top",
+        end: "+=500px",
+        scrub: true,
+        markers: true,
+      },
+    })
+  }, []);
+
   return (
     <div className={styles.intro}>
       <div className={styles.backgroundImage}>
