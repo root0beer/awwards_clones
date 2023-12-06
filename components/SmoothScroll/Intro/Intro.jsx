dd28eec02d328dafbd14b606934a1999a57b88e3
root0beer <tanyajackdaw@gmail.com>
2023-12-06 17:10:36 +0200

    locomotive scroll faces issues

--- components/SmoothScroll/Intro/Intro.jsx
+++ components/SmoothScroll/Intro/Intro.jsx
@@ -1,27 +1,40 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef, useEffect } from "react";
 import styles from "./Intro.module.scss";
 import Image from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
 
 const Intro = () => {
 
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+  // useLayoutEffect(() => {
+  //   gsap.registerPlugin(ScrollTrigger);
 
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: document.documentElement,
-        start: "top",
-        end: "+=500px",
-        scrub: true,
-        markers: true,
-      },
-    })
-  }, []);
+  //   const timeline = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: document.documentElement,
+  //       start: "top",
+  //       end: "+=500px",
+  //       scrub: true,
+  //       markers: true,
+  //     },
+  //   })
+  // }, []);
+
+  const containerRef = useRef(null);
+  const { scroll } = useLocomotiveScroll({
+    container: containerRef.current,
+    // Add other Locomotive Scroll options if needed
+  });
+
+  useEffect(() => {
+    if (scroll) {
+      scroll.on("scroll", ScrollTrigger);
+    }
+  }, [scroll]);
 
   return (
-    <div className={styles.intro}>
+    <div className={styles.intro} ref={containerRef}>
       <div className={styles.backgroundImage}>
         <Image
           src={"/smoothscroll/background.jpeg"}
