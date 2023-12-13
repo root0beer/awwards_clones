e933caa95a53fc0cf467438856be06418a299c5a
root0beer <tanyajackdaw@gmail.com>
2023-12-13 12:53:47 +0200

    rendering planes

--- components/FloatingImages/FloatingImages.jsx
+++ components/FloatingImages/FloatingImages.jsx
@@ -10,9 +10,33 @@ import {
   Floating7,
   Floating8,
 } from "./data";
+import Image from "next/image";
 
 const FloatingImages = () => {
-  return <main className={styles.main}></main>;
+  return (
+    <div className={styles.content}>
+      <main className={styles.main}>
+        <div className={styles.plane}>
+          <Image src={Floating4} alt="image" width={250} />
+          <Image src={Floating6} alt="image" width={250} />
+          <Image src={Floating8} alt="image" width={228} />
+        </div>
+        <div className={styles.plane}>
+          <Image src={Floating1} alt="image" width={300} />
+          <Image src={Floating2} alt="image" width={300} />
+          <Image src={Floating3} alt="image" width={300} />
+        </div>
+        <div className={styles.plane}>
+          <Image src={Floating5} alt="image" width={200} />
+          <Image src={Floating7} alt="image" width={200} />
+        </div>
+        <div className={styles.description}>
+          <h1>Floating Images Gallery</h1>
+          <p>Made with Next.js and GSAP</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default FloatingImages;
--- components/FloatingImages/data.js
+++ components/FloatingImages/data.js
@@ -1,11 +1,11 @@
-import Floating1 from "../public/floatingimg/floating_1.jpg";
-import Floating2 from "../public/floatingimg/floating_2.jpg";
-import Floating3 from "../public/floatingimg/floating_3.jpg";
-import Floating4 from "../public/floatingimg/floating_4.jpg";
-import Floating5 from "../public/floatingimg/floating_5.jpg";
-import Floating6 from "../public/floatingimg/floating_6.jpg";
-import Floating7 from "../public/floatingimg/floating_7.jpg";
-import Floating8 from "../public/floatingimg/floating_8.jpg";
+import Floating2 from "../../public/floatingimg/floating_2.jpg";
+import Floating1 from "../../public/floatingimg/floating_1.jpg";
+import Floating3 from "../../public/floatingimg/floating_3.jpg";
+import Floating4 from "../../public/floatingimg/floating_4.jpg";
+import Floating5 from "../../public/floatingimg/floating_5.jpg";
+import Floating6 from "../../public/floatingimg/floating_6.jpg";
+import Floating7 from "../../public/floatingimg/floating_7.jpg";
+import Floating8 from "../../public/floatingimg/floating_8.jpg";
 
 export {
     Floating1,
