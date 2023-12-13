615b8f846271d1b192eadca89daa831a548f70d0
root0beer <tanyajackdaw@gmail.com>
2023-12-13 11:28:26 +0200

    importing images

--- components/FloatingImages/FloatingImages.jsx
+++ components/FloatingImages/FloatingImages.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 import styles from "./FloatingImages.module.scss";
+import {
+  Floating1,
+  Floating2,
+  Floating3,
+  Floating4,
+  Floating5,
+  Floating6,
+  Floating7,
+  Floating8,
+} from "./data";
 
 const FloatingImages = () => {
-  return (
-    <div>FloatingImages</div>
-  )
+  return <main className={styles.main}></main>;
 };
 
 export default FloatingImages;
---
+++ components/FloatingImages/data.js
@@ -0,0 +1,19 @@
+import Floating1 from "../public/floatingimg/floating_1.jpg";
+import Floating2 from "../public/floatingimg/floating_2.jpg";
+import Floating3 from "../public/floatingimg/floating_3.jpg";
+import Floating4 from "../public/floatingimg/floating_4.jpg";
+import Floating5 from "../public/floatingimg/floating_5.jpg";
+import Floating6 from "../public/floatingimg/floating_6.jpg";
+import Floating7 from "../public/floatingimg/floating_7.jpg";
+import Floating8 from "../public/floatingimg/floating_8.jpg";
+
+export {
+    Floating1,
+    Floating2,
+    Floating3,
+    Floating4,
+    Floating5,
+    Floating6,
+    Floating7,
+    Floating8,
+};
