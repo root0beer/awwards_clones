975799e90834ee23812d26875e480073eeffc406
root0beer <tanyajackdaw@gmail.com>
2023-12-14 11:07:17 +0200

    adding animation

--- components/FloatingImages/FloatingImages.jsx
+++ components/FloatingImages/FloatingImages.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import styles from "./FloatingImages.module.scss";
 import {
   Floating1,
@@ -11,22 +11,36 @@ import {
   Floating8,
 } from "./data";
 import Image from "next/image";
+import gsap from "gsap";
 
 const FloatingImages = () => {
+
+  const plane1 = useRef(null);
+  const plane2 = useRef(null);
+  const plane3 = useRef(null);
+  const speed = 0.1;
+
+  const manageMouseMove = (e) => {
+    const {movementX, movementY} = e;
+    gsap.set(plane1.current, {x: `+=${movementX * speed}`, y: `+=${movementY * speed}`});
+    gsap.set(plane2.current, {x: `+=${movementX * speed * 0.5}`, y: `+=${movementY * speed * 0.5}`});
+    gsap.set(plane3.current, {x: `+=${movementX * speed * 0.25}`, y: `+=${movementY * speed * 0.25}`});
+  };
+
   return (
     <div className={styles.content}>
-      <main className={styles.main}>
-        <div className={styles.plane}>
+      <main onMouseMove={(e) => {manageMouseMove(e)}} className={styles.main}>
+        <div ref={plane1} className={styles.plane}>
           <Image src={Floating4} alt="image" width={250} />
           <Image src={Floating6} alt="image" width={250} />
           <Image src={Floating8} alt="image" width={228} />
         </div>
-        <div className={styles.plane}>
+        <div ref={plane2} className={styles.plane}>
           <Image src={Floating1} alt="image" width={300} />
           <Image src={Floating2} alt="image" width={300} />
           <Image src={Floating3} alt="image" width={300} />
         </div>
-        <div className={styles.plane}>
+        <div ref={plane3} className={styles.plane}>
           <Image src={Floating5} alt="image" width={200} />
           <Image src={Floating7} alt="image" width={200} />
         </div>
