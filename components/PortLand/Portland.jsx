ea24f5e0a0f3825d0834699c8de340fa888e3834
root0beer <tanyajackdaw@gmail.com>
2023-12-14 12:39:10 +0200

    project initialization

---
+++ components/PortLand/Portland.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Portland.module.scss";
+
+const Portland = () => {
+  return (
+    <div>Portland</div>
+  )
+};
+
+export default Portland;
---
+++ pages/portland/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Portland from "@/components/PortLand/Portland";
+import Wrapper from "@/components/Wrapper/Wrapper";
+import React from "react";
+
+const PortLandPage = () => {
+  return (
+    <Wrapper>
+      <Navbar />
+      <Portland />
+    </Wrapper>
+  );
+};
+
+export default PortLandPage;
